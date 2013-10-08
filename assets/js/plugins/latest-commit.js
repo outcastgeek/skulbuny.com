15760e88e06a70e2aab274c50040de3509e1e2d5
Sean Clayton <sean@skulbuny.com>
2013-10-08 04:35:10 -0400

    Added comments and made variables more semantic

--- assets/js/plugins/latest-commit.js
+++ assets/js/plugins/latest-commit.js
@@ -1,32 +1,35 @@
+// Written by Sean Clayton
+// 2013-10-08
+// http://github.com/skulbuny/latest-commit.js
 jQuery(document).ready(function($){
-  $('.latest-commit').each(function(){
-    var $container = $(this), $widget,
-      repo = $container.data('repo'),
-      vendorName = repo.split('/')[0],
+  $('.latest-commit').each(function(){ // Attach to any div/section/whatever with this class
+    var $container = $(this), $commit,
+      repo = $container.data('github'),
+      username = repo.split('/')[0],
       repoName = repo.split('/')[1],
-      vendorUrl = "http://github.com/" + vendorName,
-      repoUrl = "http://github.com/" + vendorName + '/' + repoName;
-      $widget = $(
-        '<div>' +
+      userUrl = "http://github.com/" + username, // Gets your user url
+      repoUrl = "http://github.com/" + username + '/' + repoName; // Gets your repo url
+      $commit = $(
+        '<div>' + // Needs to be wrapped.
         '<p>Changed File: <a class="commit-file" href="#" target="_blank"></a></p>' +
         '<span>Commit Message: </span><span class="commit"></span>' +
         '<p><a class="btn btn-info commit-link" href="#" target="_blank"></a></p>' +
         '</div>'
       );
-    $widget.appendTo($container);
+    $commit.appendTo($container);
     $.ajax({
-      url: 'https://api.github.com/repos/' + repo + '/commits/master',
+      url: 'https://api.github.com/repos/' + repo + '/commits/master', // Sets url to your commit index
       dataType: 'jsonp',
       success: function(results) {
         var repo = results.data, date, pushed_at = 'unknown';
-        var commitUrl = repo.html_url;
-        var fullsha = repo.sha;
-        var sha = fullsha.substring(0,10);
-        var file = repo.files[0].filename;
-        var fileUrl = repo.files[0].blob_url;
-        $widget.find('.commit').text(repo.commit.message);
-        $widget.find('.commit-file').attr('href',fileUrl).text(file).append(" <i class='icon-file-text'></i>");
-        $widget.find('.commit-link').attr('href',commitUrl).text(sha).append(" <i class='icon-large icon-circle-arrow-right'></i>");
+        var commitUrl = repo.html_url; // Grabs URL of the commit
+        var fullsha = repo.sha; // Grabs SHA of Commit
+        var sha = fullsha.substring(0,10); // Truncates SHA to 10 characters, just like GitHub does
+        var file = repo.files[0].filename; // Grabs the first filename
+        var fileUrl = repo.files[0].blob_url; // URL to the aforementioned file
+        $commit.find('.commit').text(repo.commit.message); // Outputs commit message
+        $commit.find('.commit-file').attr('href',fileUrl).text(file).append(" <i class='icon-file-text'></i>"); //
+        $commit.find('.commit-link').attr('href',commitUrl).text(sha).append(" <i class='icon-large icon-circle-arrow-right'></i>");
       }
     });
   });
